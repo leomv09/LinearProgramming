50399054d29e3a8ff74d00fc89c8098a9a11cf90
José Andrés García <jags9415@gmail.com>
2015-03-09 12:30:55 -0600

    Add more tests

--- test/cr/tec/lpsolver/LPSolverTest.java
+++ test/cr/tec/lpsolver/LPSolverTest.java
@@ -19,7 +19,7 @@ public class LPSolverTest {
      * Test of solve method, of class LPSolver.
      */
     @Test
-    public void testSolve() {
+    public void test1() {
         System.out.println("Solve basic LP problem.");
         System.out.println("MAX z = x + y");
         System.out.println("Suj:");
@@ -39,9 +39,42 @@ public class LPSolverTest {
         Result result = solver.solve(context, ProblemType.MAX);
         
         System.out.println(result);
+        System.out.println();
         
         assertEquals(result.getValue(0), 4.0, 0);
         assertEquals(result.getValue(1), 4.0, 0);
+    }
+    
+    /**
+     * Test of solve method, of class LPSolver.
+     */
+    @Test
+    public void test2() {
+        System.out.println("Solve basic LP problem.");
+        System.out.println("MAX z = -2x + 5y");
+        System.out.println("Suj:");
+        System.out.println("100 <= x <= 200");
+        System.out.println("80 <= y <= 170");
+        System.out.println("y >= -x + 200");
+
+        ObjetiveFunction function = new ObjetiveFunction(new double[] {-2, 5});
+        
+        Constraint[] constraints = new Constraint[] {
+            new Constraint(new double[] {1}, Relationship.GEQ, 100),
+            new Constraint(new double[] {1}, Relationship.LEQ, 200),
+            new Constraint(new double[] {0, 1}, Relationship.GEQ, 80),
+            new Constraint(new double[] {0, 1}, Relationship.LEQ, 170),
+            new Constraint(new double[] {1, 1}, Relationship.GEQ, 200)
+        };
+        
+        LPContext context = new LPContext(function, constraints);
+        Result result = solver.solve(context, ProblemType.MAX);
+        
+        System.out.println(result);
+        System.out.println();
+        
+        assertEquals(result.getValue(0), 100.0, 0);
+        assertEquals(result.getValue(1), 170.0, 0);
     }
     
 }
