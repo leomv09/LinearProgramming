e3c3ee546a81c5926f83d98ab5e07a4cd7954cee
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-03-12 14:20:57 -0600

    Create classes FeasibleRegion and GraphicalSolver

---
+++ src/cr/tec/lpsolver/FeasibleRegion.java
@@ -0,0 +1,32 @@
+package cr.tec.lpsolver;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import java.util.Set;
+
+/**
+ *
+ * @author Leo
+ */
+public class FeasibleRegion {
+    
+    public FeasibleRegion(Constraint constraint) {
+        
+    }
+    
+    public void intersect(FeasibleRegion r) {
+        
+    }
+    
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean isBounded() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public Set<Coordinate> getVertex() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ src/cr/tec/lpsolver/GraphicalSolver.java
@@ -0,0 +1,14 @@
+package cr.tec.lpsolver;
+
+/**
+ *
+ * @author Leo
+ */
+public class GraphicalSolver implements Solver {
+
+    @Override
+    public Result solve(Problem problem) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
