57339c5a31aec8a044e3bda148d486c89d75a33b
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-03-22 00:29:48 -0600

    GraphicalSolver implemented. Not tested yet.

--- src/cr/tec/lpsolver/GraphicalSolver.java
+++ src/cr/tec/lpsolver/GraphicalSolver.java
@@ -1,14 +1,101 @@
 package cr.tec.lpsolver;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import math.geom2d.Point2D;
+
 /**
  *
  * @author Leo
  */
 public class GraphicalSolver implements Solver {
 
+    /**
+     * Solve a optimization problem.
+     *
+     * @param problem The optimization problem.
+     * 
+     * @return The result of the problem or {@code null} if there exists no
+     * feasible solution.
+     */
     @Override
     public Result solve(Problem problem) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        FeasibleRegion region = null;
+        
+        for(Constraint cons : problem.getConstraints())
+        {
+            if(region == null)
+            {
+                region = new FeasibleRegion(problem.getVariables(), cons);
+            }
+            else
+            {
+                FeasibleRegion region2 = new FeasibleRegion(problem.getVariables(), cons);
+                region = region.intersection(region2);
+            }
+        }
+        
+        if(region != null)
+        {
+            Point2D optimumPoint = getMinMax(region.getVertex(), problem.getProblemType(), problem.getObjetiveFunction());
+            Map<String, Double> values = new HashMap();
+            values.put("x", optimumPoint.getX());
+            values.put("y", optimumPoint.getY());
+        
+            return new Result(region, problem.getObjetiveFunction().evaluate(values), optimumPoint);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    
+    /**
+     * Obtains the optimum point (min or max) of a feasible region.
+     * @param vertex A list of vertex from the feasible region.
+     * @param type The problem type (min or max).
+     * @param objectiveFunction The Linear objective function of the problem.
+     * @return The optimum Point2D (point) of the feasible region.
+     */
+    private Point2D getMinMax(Collection<Point2D> vertex, ProblemType type, Linear objectiveFunction)
+    {
+        Point2D currentPoint = null;
+        double currentValue;
+        if(type == ProblemType.MAX)
+        {
+            currentValue = 0.0;
+            for(Point2D point : vertex)
+            {
+                Map<String, Double> values = new HashMap();
+                values.put("x", point.getX());
+                values.put("y", point.getY());
+                if(currentValue < objectiveFunction.evaluate(values))
+                {
+                    currentPoint = point;
+                    currentValue = objectiveFunction.evaluate(values);
+                }
+            }
+            return currentPoint;
+        }
+        else
+        {
+            currentValue = Double.POSITIVE_INFINITY;
+            for(Point2D point : vertex)
+            {
+                Map<String, Double> values = new HashMap();
+                values.put("x", point.getX());
+                values.put("y", point.getY());
+                if(currentValue > objectiveFunction.evaluate(values))
+                {
+                    currentPoint = point;
+                    currentValue = objectiveFunction.evaluate(values);
+                }
+            }
+            return currentPoint;
+        }  
     }
     
 }
--- src/cr/tec/lpsolver/Result.java
+++ src/cr/tec/lpsolver/Result.java
@@ -1,11 +1,56 @@
 package cr.tec.lpsolver;
 
+import math.geom2d.Point2D;
+
 /**
  * The {@code Result} of a {@link Problem}.
  * 
- * @author jose
+ * @author Leo
  */
 public class Result {
     
-
+    FeasibleRegion feasibleRegion;//Feasible region of the problem.
+    double optimumValue;//Best result depending on its type(min or max).
+    Point2D optimumPoint;//Best point of the feasible region.
+    
+    
+    /**
+     * Creates a new instance of Result. 
+     * @param region The feasible region of the problem.
+     * @param OptimumValue The best result at the moment of evaluating the points.
+     * @param OptimumPoint The best point of the feasible region.
+     */
+    public Result(FeasibleRegion region, double OptimumValue, Point2D OptimumPoint)
+    {
+        this.feasibleRegion = region;
+        this.optimumValue = OptimumValue;
+        this.optimumPoint = OptimumPoint;
+    }
+    
+    /**
+     * Obtains the feasible region of the prroblem's result.
+     * @return feasible region of the problem.
+     */
+    public FeasibleRegion getFeasibleRegion()
+    {
+        return this.feasibleRegion;
+    }
+    
+    /**
+     * Obtains the optimum value of the problem.
+     * @return The double optimum value.
+     */
+    public double getOptimumResult()
+    {
+        return this.optimumValue;
+    }
+    
+    /**
+     * Obtains the optimum point of the feasible region.
+     * @return the best Point2D (point) of the feasible region.
+     */
+    public Point2D getOptimumPoint()
+    {
+        return this.optimumPoint;
+    }
 }
