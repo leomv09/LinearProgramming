d58f5ed577ec936cc04ca0d3575d2818de54bb0d
José Andrés García <jags9415@gmail.com>
2015-06-01 16:22:32 -0600

    Create transport solver templates

---
+++ src/cr/tec/lpsolver/transport/DefaultTransportSolver.java
@@ -0,0 +1,14 @@
+package cr.tec.lpsolver.transport;
+
+/**
+ *
+ * @author José Andrés García Sáenz <jags9415@gmail.com>
+ */
+public class DefaultTransportSolver implements TransportSolver {
+
+    @Override
+    public TransportResult solve(TransportProblem problem) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
