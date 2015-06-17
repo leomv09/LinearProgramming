63abda97b823707bdfefb941b2291a58d6c8744d
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-06-16 20:39:33 -0600

    Shortest path method update.

---
+++ src/cr/tec/dynamic/Edge.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cr.tec.dynamic;
+
+/**
+ *
+ * @author Leo
+ */
+public class Edge {
+    private final Node startNode;
+    private final Node endNode;
+    private final double wight;
+
+    public Edge(Node start, Node end, double Weight)
+    {
+        this.startNode = start;
+        this.endNode = end;
+        this.wight = Weight;
+    }
+    
+    public Node getStartNode() {
+        return startNode;
+    }
+
+    public Node getEndNode() {
+        return endNode;
+    }
+
+    public double getWight() {
+        return wight;
+    }   
+}
---
+++ src/cr/tec/dynamic/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cr.tec.dynamic;
+
+/**
+ *
+ * @author Leo
+ */
+public class Node {
+    private final String name;
+    
+    public Node(String Name)
+    {
+        this.name = Name;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+}
